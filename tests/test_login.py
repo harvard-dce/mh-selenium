42ecc2059d96375f5697f78ab737d4c718a7d2ba
Jay Luker <jay_luker@harvard.edu>
2017-03-28 16:07:54 -0400

    adding manual login test example

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,3 +1,24 @@
 
-def test_admin_login(admin_browser):
+def test_admin_login_fixture(admin_browser):
     assert 'Release' in admin_browser.title
+
+
+def test_admin_login(browser, opt_getter):
+
+    # get the login deets
+    admin_host = opt_getter('admin_host')
+    admin_user = opt_getter('admin_user')
+    admin_pass = opt_getter('admin_pass')
+
+    # go to the admin site
+    browser.visit(admin_host)
+
+    # fill in the user/pass
+    browser.fill('j_username', admin_user)
+    browser.fill('j_password', admin_pass)
+
+    # click submit
+    browser.find_by_name('submit').click()
+
+    assert 'Release' in browser.title
+    return
